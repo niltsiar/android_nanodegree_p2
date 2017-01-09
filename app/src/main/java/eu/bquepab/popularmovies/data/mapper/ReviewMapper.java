5f59c65ee2a0967c2b0b91b29ed6b6dcd19160f3
Borja Quevedo Pablos <borjota@gmail.com>
2017-01-09 13:33:46 +0100

    Mappers

---
+++ app/src/main/java/eu/bquepab/popularmovies/data/mapper/ReviewMapper.java
@@ -0,0 +1,29 @@
+package eu.bquepab.popularmovies.data.mapper;
+
+import eu.bquepab.popularmovies.data.model.RealmReview;
+import eu.bquepab.popularmovies.model.Review;
+
+public class ReviewMapper {
+
+    //No instances
+    private ReviewMapper() {
+    }
+
+    public static RealmReview map(final Review review) {
+        return RealmReview.builder()
+                          .id(review.id())
+                          .author(review.author())
+                          .content(review.content())
+                          .url(review.url())
+                          .build();
+    }
+
+    public static Review map(final RealmReview review) {
+        return Review.builder()
+                     .id(review.getId())
+                     .author(review.getAuthor())
+                     .content(review.getContent())
+                     .url(review.getUrl())
+                     .build();
+    }
+}
---
+++ app/src/main/java/eu/bquepab/popularmovies/data/mapper/TrailerMapper.java
@@ -0,0 +1,33 @@
+package eu.bquepab.popularmovies.data.mapper;
+
+import eu.bquepab.popularmovies.data.model.RealmTrailer;
+import eu.bquepab.popularmovies.model.Trailer;
+
+public class TrailerMapper {
+
+    //No instances
+    private TrailerMapper() {
+    }
+
+    public static RealmTrailer map(final Trailer trailer) {
+        return RealmTrailer.builder()
+                           .id(trailer.id())
+                           .key(trailer.key())
+                           .name(trailer.name())
+                           .site(trailer.site())
+                           .size(trailer.size())
+                           .type(trailer.type())
+                           .build();
+    }
+
+    public static Trailer map(final RealmTrailer trailer) {
+        return Trailer.builder()
+                      .id(trailer.getId())
+                      .key(trailer.getKey())
+                      .name(trailer.getName())
+                      .site(trailer.getSite())
+                      .size(trailer.getSize())
+                      .type(trailer.getType())
+                      .build();
+    }
+}
